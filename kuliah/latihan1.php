d3fc60e7e893981adebf89088d78827b71884c32
ritsadhil <haritsfadhil884@gmail.com>
2024-02-25 12:55:48 +0700

    catatan pertemuan 3

---
+++ kuliah/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!";
+echo "<br>";
+echo "Kuliah Pemrograman Web";
+echo "<br>";
+echo 123 + 10;
+//untuk penjumlahan tidak perlu kutip ("")
+echo 3.14;
+echo "<br>";
+echo true;
+//true = 1
+echo false;
+//false = 0 (di web hanya ada kosong/tidak ada apa apa)
+echo "<br>";
+echo "jum'at";
+echo "<br>";
+echo '"Fadhil" kuliah hari ' . "jum'at";
---
+++ kuliah/latihan2.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "Fadhil";
+$univ = "Universitas Pasundan";
+$umur = 20;
+echo "<p>Halo,nama saya $nama, saya $umur tahun, kuliah di $univ </p>";
